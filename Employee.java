946b794248990a5b523c77a4ccd68c1844266403
Ishu-81 <24wh1a0581@bvrithyderabad.edu.in>
2026-04-10 13:57:14 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,35 @@
+package ebook581;
+
+
+public class Employee {
+
+	private int empid;
+
+
+	private String name;
+
+
+	private int salary;
+
+
+	private String department;
+
+
+	public Employee(int id, String name, int salary, String company) {
+
+
+		this.empid = id;
+
+
+		this.name = name;
+
+
+		this.salary = salary;
+
+
+		this.department = company;
+
+	}
+
+
+}
